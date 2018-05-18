420fb6b265afb2049291f2952a59aa5be40f4f75
Alex <xia.alex.t@gmail.com>
2018-05-18 15:17:31 -0500

    boop

--- Deck.java
+++ Deck.java
@@ -2,26 +2,35 @@ import java.util.*;
 public class Deck{
     private ArrayList<Card> deck;
     public Deck(){
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < 4; i++){ //adds Wild
             deck.add(new Card("Wild"));
         }
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < 4; i++){ //adds +4
             deck.add(new Card("+4"));
         }
-        double redC = 1;
-        for(int i = 0; i < 18; i++){
-            int temp = (int)redC;
+        double counter = 1;
+        for(int i = 0; i < 18; i++){ //adds red 1-9
+            int temp = (int)counter;
             deck.add(new Card(temp, "Red"));
-            redC += .5;
+            counter += .5;
         }
-        for(int i = 0; i < 2; i++){
-            deck.add(new Card(1, "Blue"));
+        counter = 1;
+        for(int i = 0; i < 18; i++){ //adds blue 1-9
+            int temp = (int)counter;
+            deck.add(new Card(temp, "Blue"));
+            counter += .5;
         }
-        for(int i = 0; i < 2; i++){
-            deck.add(new Card(1, "Green"));
+        counter = 1;
+        for(int i = 0; i < 18; i++){ //adds green 1-9
+            int temp = (int)counter;
+            deck.add(new Card(temp, "Green"));
+            counter += .5;
         }
-        for(int i = 0; i < 2; i++){
-            deck.add(new Card(1, "Yellow"));
+        counter = 1;
+        for(int i = 0; i < 18; i++){ //adds yellow 1-9
+            int temp = (int)counter;
+            deck.add(new Card(temp, "Yellow"));
+            counter += .5;
         }
         for(int i = 0; i < 2; i++){
             deck.add(new Card(2, ""));

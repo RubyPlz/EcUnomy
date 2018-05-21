d4882db34c0d26a8ff76bf687b73dcc59d920dc2
Arjun Pal <anonamause10@gmail.com>
2018-05-21 10:20:22 -0500

    added a thicc enum

---
+++ Type.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Type - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Type
+{
+    WILD, PLUS4, REVERSE, SKIP, PLUS2, PLUS2ALL, SWAPHANDS, COMMUNISM, NOU;
+}
